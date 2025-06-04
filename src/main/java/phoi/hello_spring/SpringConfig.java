4164d9dd78cc8bbedb2e18b38b2345700a7c5100
phoi5675 <phoi5675@gmail.com>
2025-06-04 14:08:33 +0900

    AOP 예시 - 시간 측정 예시 추가

--- src/main/java/phoi/hello_spring/SpringConfig.java
+++ src/main/java/phoi/hello_spring/SpringConfig.java
@@ -1,12 +1,9 @@
 package phoi.hello_spring;
 
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import phoi.hello_spring.repository.JpaMemberRepository;
 import phoi.hello_spring.repository.MemberRepository;
-import phoi.hello_spring.repository.MemoryMemberRepository;
 import phoi.hello_spring.service.MemberService;
 
 @Configuration
@@ -22,10 +19,4 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
-
-//    @Bean
-//    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JpaMemberRepository(em);
-//    }
 }
