17925f15777d70fea718d902cf1a61642192d009
phoi5675 <phoi5675@gmail.com>
2025-06-16 20:45:02 +0900

    싱글톤 관련 예시 및 테스트 코드 추가
    - 싱글톤을 사용하면 안되는 예시 관련 코드 추가 : stateful한 파라미터가 존재하는 싱글톤 객체에 대한 예시 추가

--- src/test/java/springcore/core/singleton/SingletonTest.java
+++ src/test/java/springcore/core/singleton/SingletonTest.java
@@ -3,6 +3,8 @@ package springcore.core.singleton;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springcore.core.AppConfig;
 import springcore.core.member.MemberService;
 
@@ -36,5 +38,22 @@ public class SingletonTest {
         System.out.println("singletonService2 = " + singletonService2);
 
         Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 1. 조회 : 호출할 때마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService1 = " + memberService2);
+
+        // memberService1 != memberService2
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
