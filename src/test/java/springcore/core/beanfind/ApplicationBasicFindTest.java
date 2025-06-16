5b6ede5aa10a27af08a8aa3b474da51fbf2371d6
phoi5675 <phoi5675@gmail.com>
2025-06-16 14:32:08 +0900

    스프링 빈 관련 테스트 코드 추가
    - 빈 이름으로 조회하는 방법
    - 빈 이름이 중복되는 경우 관련 테스트 코드 추가

---
+++ src/test/java/springcore/core/beanfind/ApplicationBasicFindTest.java
@@ -0,0 +1,45 @@
+package springcore.core.beanfind;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import springcore.core.AppConfig;
+import springcore.core.member.MemberService;
+import springcore.core.member.MemberServiceImpl;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanBySpecificName() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회되지 않음")
+    void findBeanByNameX() {
+//        MemberService xxxx = ac.getBean("xxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxx", MemberService.class));
+    }
+}
