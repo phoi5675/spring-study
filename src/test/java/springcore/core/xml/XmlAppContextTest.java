91f7492177bdc3979a6f913026fb406d60ff7444
phoi5675 <phoi5675@gmail.com>
2025-06-16 15:50:04 +0900

    XML으로 빈 등록하는 방법 관련 테스트 코드 추가

---
+++ src/test/java/springcore/core/xml/XmlAppContextTest.java
@@ -0,0 +1,17 @@
+package springcore.core.xml;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import springcore.core.member.MemberService;
+
+public class XmlAppContextTest {
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+}
