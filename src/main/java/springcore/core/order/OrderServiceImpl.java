127017f576183bc2e875037eb322fd3478a56f5c
phoi5675 <phoi5675@gmail.com>
2025-06-24 20:15:59 +0900

    Lombok 라이브러리 추가

--- src/main/java/springcore/core/order/OrderServiceImpl.java
+++ src/main/java/springcore/core/order/OrderServiceImpl.java
@@ -1,21 +1,16 @@
 package springcore.core.order;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import springcore.core.discount.DiscountPolicy;
 import springcore.core.member.Member;
 import springcore.core.member.MemberRepository;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
