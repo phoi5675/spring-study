4a00ac6cbe56230d1991e2f8a2fad28c644b66d7
phoi5675 <phoi5675@gmail.com>
2025-06-25 16:57:19 +0900

    프로토타입 스코프 빈 관련 예시 코드 추가

---
+++ src/test/java/springcore/core/scope/PrototypeTest.java
@@ -0,0 +1,37 @@
+package springcore.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class PrototypeTest {
+    @Test
+    void prototypeBeanFine() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean singletonBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean singletonBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        Assertions.assertThat(singletonBean1).isNotSameAs(singletonBean2);
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean destroy");
+        }
+    }
+}
