eec0f323704f8c1dce65efd784cb30906544e822
phoi5675 <phoi5675@gmail.com>
2025-06-30 21:23:58 +0900

    Initial commit

---
+++ src/main/java/jpabasic/Member.java
@@ -0,0 +1,27 @@
+package jpabasic;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Id
+    private long id;
+    private String name;
+}
