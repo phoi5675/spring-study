2e36f735d710899db35f89a01eb6ef67ea69c6c4
phoi5675 <phoi5675@gmail.com>
2025-07-04 15:39:02 +0900

    1:N 관계에서 연관관계 매핑 관련 예시 추가

--- src/main/java/jpabasic/JpaMain.java
+++ src/main/java/jpabasic/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -24,9 +25,15 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
-            Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam);
+            em.flush();
+            em.clear();
+
+            Team findteam = em.find(Team.class, team.getId());
+            List<Member> members = findteam.getMembers();
+
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
 
             tx.commit();
         } catch (Exception e) {
