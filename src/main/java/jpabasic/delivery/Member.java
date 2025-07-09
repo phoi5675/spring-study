f056bd41e64384891261d228a9c073acafea7f0b
phoi5675 <phoi5675@gmail.com>
2025-07-09 14:22:28 +0900

    임베디드 타입 관련 예시 추가

--- src/main/java/jpabasic/delivery/Member.java
+++ src/main/java/jpabasic/delivery/Member.java
@@ -1,24 +1,36 @@
 package jpabasic.delivery;
 
-import jpabasic.employee.BaseEntity;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-public class Member extends BaseEntity {
+public class Member {
     @Id
     @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
 
-    private String name;
+    @Column(name = "USERNAME")
+    private String username;
 
-    private String city;
+    @Embedded
+    private Period workPeriod;
 
-    private String street;
+    @Embedded
+    private Address address;
 
-    private String zipcode;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
