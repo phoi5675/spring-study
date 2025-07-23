d12a88fb0392a9220c9938bb749ca292190c59b6
phoi5675 <phoi5675@gmail.com>
2025-07-23 16:35:18 +0900

    공통 컨트롤러 관련 실습 예시 코드 추가

---
+++ spring-mvc/servlet/src/main/java/spring_mvc/servlet/web/frontcontroller/v1/controller/MemberSaveController.java
@@ -0,0 +1,30 @@
+package spring_mvc.servlet.web.frontcontroller.v1.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import spring_mvc.servlet.domain.member.Member;
+import spring_mvc.servlet.domain.member.MemberRepository;
+import spring_mvc.servlet.web.frontcontroller.v1.ControllerV1;
+
+import java.io.IOException;
+
+public class MemberSaveController implements ControllerV1 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        req.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+    }
+}
