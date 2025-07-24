22dc2b36d03d9bf032c30cca888885856aeaf5f0
phoi5675 <phoi5675@gmail.com>
2025-07-24 11:13:23 +0900

    Model / View 분리 예시 코드 추가

---
+++ spring-mvc/servlet/src/main/java/spring_mvc/servlet/web/frontcontroller/v4/controller/MemberFormControllerV4.java
@@ -0,0 +1,14 @@
+package spring_mvc.servlet.web.frontcontroller.v4.controller;
+
+import spring_mvc.servlet.web.frontcontroller.ModelView;
+import spring_mvc.servlet.web.frontcontroller.v3.ControllerV3;
+import spring_mvc.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.Map;
+
+public class MemberFormControllerV4 implements ControllerV4 {
+    @Override
+    public String process(Map<String, String> paramMap,  Map<String, Object> model) {
+        return "new-form";
+    }
+}
---
+++ spring-mvc/servlet/src/main/java/spring_mvc/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
@@ -0,0 +1,25 @@
+package spring_mvc.servlet.web.frontcontroller.v4.controller;
+
+import spring_mvc.servlet.domain.member.Member;
+import spring_mvc.servlet.domain.member.MemberRepository;
+import spring_mvc.servlet.web.frontcontroller.ModelView;
+import spring_mvc.servlet.web.frontcontroller.v3.ControllerV3;
+import spring_mvc.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.Map;
+
+public class MemberSaveControllerV4 implements ControllerV4 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap,  Map<String, Object> model) {
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.put("member", member);
+        return "save-result";
+    }
+}
