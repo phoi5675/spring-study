ae3b64d74ea7329a8bb4c6ecd3a4cdd8eee877ba
phoi5675 <phoi5675@gmail.com>
2025-07-26 21:18:59 +0900

    스프링 MVC 기본 예시 코드 추가

---
+++ spring-mvc/servlet/src/main/java/spring_mvc/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,29 @@
+package spring_mvc.servlet.web.springmvc.v1;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import spring_mvc.servlet.domain.member.Member;
+import spring_mvc.servlet.domain.member.MemberRepository;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(HttpServletRequest req, HttpServletResponse resp) {
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView modelView = new ModelAndView("save-result");
+        modelView.addObject("member", member);
+
+        return modelView;
+    }
+}
