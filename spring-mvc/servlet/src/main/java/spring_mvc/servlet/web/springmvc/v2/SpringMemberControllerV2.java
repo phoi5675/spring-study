f106f152361e11508ceb951538a3065831bf398b
phoi5675 <phoi5675@gmail.com>
2025-07-26 21:25:00 +0900

    스프링 MVC 컨트롤러 통합 예시 코드 추가

---
+++ spring-mvc/servlet/src/main/java/spring_mvc/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,46 @@
+package spring_mvc.servlet.web.springmvc.v2;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import spring_mvc.servlet.domain.member.Member;
+import spring_mvc.servlet.domain.member.MemberRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping
+    public ModelAndView members() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView modelView = new ModelAndView("members");
+        modelView.addObject("members", members);
+
+        return modelView;
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest req, HttpServletResponse resp) {
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView modelView = new ModelAndView("save-result");
+        modelView.addObject("member", member);
+
+        return modelView;
+    }
+}
