30bedfad4e7570cec291230b61f5633abf3beb25
phoi5675 <phoi5675@gmail.com>
2025-08-11 15:29:37 +0900

    Item 관련 예시 코드 추가 - 상품 추가 페이지

--- spring-mvc/item-service/src/main/java/springmvc/itemservice/web/basic/BasicItemController.java
+++ spring-mvc/item-service/src/main/java/springmvc/itemservice/web/basic/BasicItemController.java
@@ -4,9 +4,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import springmvc.itemservice.domain.item.Item;
 import springmvc.itemservice.domain.item.ItemRepository;
 
@@ -41,4 +39,16 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute("item") Item item,
+                          Model model) {
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
 }
