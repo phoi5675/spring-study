4a65fbf52d1f13ff225c70fdf0ee823e4595bff7
phoi5675 <phoi5675@gmail.com>
2025-08-20 18:14:34 +0900

    로그아웃 시 쿠키 만료되도록 기능 추가

--- spring-mvc2/login/src/main/java/hello/login/web/HomeController.java
+++ spring-mvc2/login/src/main/java/hello/login/web/HomeController.java
@@ -8,12 +8,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
     private final MemberRepository memberRepository;
+
+    private static void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
 
     //    @GetMapping("/")
     public String home() {
@@ -34,5 +44,12 @@ public class HomeController {
 
         model.addAttribute("member", loginMember);
         return "loginHome";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+
+        return "redirect:/";
     }
 }
