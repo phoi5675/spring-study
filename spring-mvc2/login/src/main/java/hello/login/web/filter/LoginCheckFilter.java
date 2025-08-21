40666bb620800df8c3d4ca14770cc19e55885695
phoi5675 <phoi5675@gmail.com>
2025-08-21 14:16:34 +0900

    로그인 확인 필터 추가
    - `/items` 등 로그인이 필요한 화면을 로그인하지 않고 접근하는 경우에 `/login`으로 리디렉션되도록 변경

---
+++ spring-mvc2/login/src/main/java/hello/login/web/filter/LoginCheckFilter.java
@@ -0,0 +1,57 @@
+package hello.login.web.filter;
+
+import hello.login.web.SessionConst;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Slf4j
+public class LoginCheckFilter implements Filter {
+    private static final String[] whiteList = {"/", "/members/add", "/login", "/logout", "/css/*"};
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        try {
+            log.info("인증 체크 필터 시작 {}", requestURI);
+            if (isLoginCheckPath(requestURI)) {
+                log.info("인증 체크 로직 실행 {}", requestURI);
+
+                HttpSession session = httpRequest.getSession(false);
+                if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
+                    log.info("미인증 사용자 요청 {}", requestURI);
+
+                    // 로그인으로 리디렉션
+                    httpResponse.sendRedirect("/login?redirectURL=" + requestURI);
+                    return;
+                }
+            }
+
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw e;
+        } finally {
+            log.info("인증 체크 필터 종료 {}", requestURI);
+        }
+    }
+
+    /**
+     * 요청한 URI가 `whiteList` 에 있는지 확인하는 함수
+     *
+     * @param requestURI 요청한 URI
+     * @return `whiteList` 에 요청한 URI가 포함되는지 여부. `true` 인 경우, 화이트리스트에 포함됨.
+     */
+    private boolean isLoginCheckPath(String requestURI) {
+        return !PatternMatchUtils.simpleMatch(whiteList, requestURI);
+    }
+}
